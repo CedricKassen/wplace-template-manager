b1b0cdec5edcac8d231e9941eaefb0bff277b7da
Marlon <marlon.john@hotmail.de>
2025-09-07 15:41:29 +0200

    use Response.blob/json hooks

--- src/App.tsx
+++ src/App.tsx
@@ -30,30 +30,105 @@ function App() {
     const [canvasElement, setCanvasElement] = useState<HTMLCanvasElement | null>(null);
     const overlays = useAtomValue(overlayAtom);
 
-    const handleMessage = async (event: MessageEvent) => {
-        const { source, blob, requestId, chunk, position } = event.data;
-
-        if (source === "wplace-tile-request") {
-            window.postMessage({
-                requestId,
-                source: "overlay-renderer",
-                blob: await renderSquares(blob, overlays, chunk[0], chunk[1]),
-                chunk,
-            } as BlobEventData);
-        }
-
-        if (source === "wplace-position-request") {
-            setPosition({ position, chunk });
-        }
+    const sharedData = {
+        overlays,
+        jumpTo: {
+            chunk: null,
+            position: null,
+        },
+        pixelUrlRegex: new RegExp(
+            "^https://backend\\.wplace\\.live/s\\d+/pixel/(\\d+)/(\\d+)\\?x=(\\d+)&y=(\\d+)$",
+        ),
+        filesUrlRegex: new RegExp(
+            "^https://backend\\.wplace\\.live/files/s\\d+/tiles/(\\d+)/(\\d+)\\.png$",
+        ),
+        randomTileUrlRegex: new RegExp(
+            "^https://backend\\.wplace\\.live/s\\d+/tile/random$",
+        )
     };
 
     useEffect(() => {
+        const originals = {
+            blob: Response.prototype.blob,
+            json: Response.prototype.json,
+            arrayBuffer: Response.prototype.arrayBuffer,
+        } as const;
+
+        const handleMessage = (event: MessageEvent) => {
+            const { source, chunk, position } = event.data;
+
+            if (source === "overlay-location-service") {
+                sharedData.jumpTo.position = position;
+                sharedData.jumpTo.chunk = chunk;
+            }
+        };
+
         window.addEventListener("message", handleMessage);
+
+        Response.prototype.blob = async function (this: Response): Promise<Blob> {
+            if (!this.url || this.url.length === 0) {
+                return originals.blob.call(this);
+            }
+            if (sharedData.filesUrlRegex.test(this.url)) {
+                const m = sharedData.filesUrlRegex.exec(this.url);
+                if (m) {
+                    const [, chunkX, chunkY] = m;
+                    const origBlob = await originals.blob.call(this);
+                    const overlayBlob = await renderSquares(
+                        origBlob,
+                        sharedData.overlays,
+                        parseInt(chunkX, 10),
+                        parseInt(chunkY, 10),
+                    );
+                    return overlayBlob || origBlob;
+                }
+            }
+            return originals.blob.call(this);
+        };
+        Response.prototype.arrayBuffer = async function (this: Response): Promise<ArrayBuffer> {
+            const blob = await this.blob();
+            return blob.arrayBuffer();
+        };
+        Response.prototype.json = async function (this: Response): Promise<any> {
+            if (!this.url || this.url.length === 0) {
+                return originals.json.call(this);
+            }
+            if (sharedData.randomTileUrlRegex.test(this.url)) {
+                if (sharedData.jumpTo.chunk && sharedData.jumpTo.position) {
+                    const jumpResponse = {
+                        pixel: {
+                            x: sharedData.jumpTo.position[0],
+                            y: sharedData.jumpTo.position[1]
+                        },
+                        tile: {
+                            x: sharedData.jumpTo.chunk[0],
+                            y: sharedData.jumpTo.chunk[1]
+                        }
+                    };
+                    sharedData.jumpTo.chunk = null;
+                    sharedData.jumpTo.position = null;
+                    return jumpResponse;
+                }
+            } else if (sharedData.pixelUrlRegex.test(this.url)) {
+                const m = sharedData.pixelUrlRegex.exec(this.url);
+                if (m) {
+                    const [, chunkX, chunkY, positionX, positionY] = m;
+                    const chunk = [parseInt(chunkX, 10), parseInt(chunkY, 10)];
+                    const position = [parseInt(positionX, 10), parseInt(positionY, 10)];
+                    setPosition({ position, chunk });
+                }
+            }
+
+            return originals.json.call(this);
+        };
 
         return () => {
             window.removeEventListener("message", handleMessage);
+            Response.prototype.blob = originals.blob;
+            Response.prototype.json = originals.json;
+            Response.prototype.arrayBuffer = originals.arrayBuffer;
         };
-    }, [overlays]);
+    }, [sharedData]);
 
     useEffect(() => {
         const mutationObserver = new MutationObserver(() => {
